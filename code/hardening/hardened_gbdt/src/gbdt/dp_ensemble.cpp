772a9410f525aa4a15e839a470612aeb73b1c8c2
Yannik Potdevin <ypo@informatik.uni-kiel.de>
2022-02-18 07:56:58 +0000

    Formatted files consistently.

--- code/hardening/hardened_gbdt/src/gbdt/dp_ensemble.cpp
+++ code/hardening/hardened_gbdt/src/gbdt/dp_ensemble.cpp
@@ -13,37 +13,40 @@ extern bool VERIFICATION_MODE;
 
 using namespace std;
 
-
 /** Constructors */
 
 DPEnsemble::DPEnsemble(ModelParams *parameters) : params(parameters)
 {
-    if (parameters->privacy_budget <= 0){
+    if (parameters->privacy_budget <= 0)
+    {
         throw std::runtime_error("hardened gbdt cannot be run with pb<=0");
     }
 
     // prepare the linspace grid
-    if(is_true(params->use_grid)) {
+    if (is_true(params->use_grid))
+    {
         double grid_range = std::get<1>(params->grid_borders) - std::get<0>(params->grid_borders);
         double step_size = params->grid_step_size;
-        int grid_size = (int) grid_range / step_size;
-        this->grid = std::vector<double>(grid_size,0);
+        int grid_size = (int)grid_range / step_size;
+        this->grid = std::vector<double>(grid_size, 0);
         int counter = 0;
-        std::generate(this->grid.begin(), this->grid.end(), [&counter, &step_size]() mutable { return counter++ * step_size; });
+        std::generate(this->grid.begin(), this->grid.end(), [&counter, &step_size]() mutable
+                      { return counter++ * step_size; });
     }
 }
-    
-DPEnsemble::~DPEnsemble() {
-    for (auto tree : trees) {
+
+DPEnsemble::~DPEnsemble()
+{
+    for (auto tree : trees)
+    {
         tree.delete_tree(tree.root_node);
     }
 }
 
-
 /** Methods */
 
 void DPEnsemble::train(DataSet *dataset)
-{   
+{
     this->dataset = dataset;
     int original_length = dataset->length;
 
@@ -54,11 +57,13 @@ void DPEnsemble::train(DataSet *dataset)
     // each tree gets the full pb, as they train on distinct data
     TreeParams tree_params;
     tree_params.tree_privacy_budget = params->privacy_budget;
-    
+
     // train all trees
-    for(int tree_index = 0; tree_index < params->nb_trees;  tree_index++) {
- 
-        if(VERIFICATION_MODE) {
+    for (int tree_index = 0; tree_index < params->nb_trees; tree_index++)
+    {
+
+        if (VERIFICATION_MODE)
+        {
             VERIFICATION_LOG("Tree {0} CV-Ensemble {1}", tree_index, cv_fold_index);
         }
 
@@ -69,25 +74,32 @@ void DPEnsemble::train(DataSet *dataset)
         tree_params.delta_g = 3 * pow(params->l2_threshold, 2);
 
         // sensitivity for leaves
-        if (is_true(params->gradient_filtering) && !is_true(params->leaf_clipping)) {
+        if (is_true(params->gradient_filtering) && !is_true(params->leaf_clipping))
+        {
             // you can only "turn off" leaf clipping if GDF is enabled!
             tree_params.delta_v = params->l2_threshold / (1 + params->l2_lambda);
-        } else {
-            tree_params.delta_v = std::min((double) (params->l2_threshold / (1 + params->l2_lambda)),
-                    2 * params->l2_threshold * pow(1-params->learning_rate, tree_index));
+        }
+        else
+        {
+            tree_params.delta_v = std::min((double)(params->l2_threshold / (1 + params->l2_lambda)),
+                                           2 * params->l2_threshold * pow(1 - params->learning_rate, tree_index));
         }
 
         // determine number of rows
         int number_of_rows = 0;
-        if (is_true(params->balance_partition)) {
+        if (is_true(params->balance_partition))
+        {
             // num_unused_rows / num_remaining_trees
             number_of_rows = dataset->length / (params->nb_trees - tree_index);
-        } else {
+        }
+        else
+        {
             // line 8 of Algorithm 2 from the paper
             number_of_rows = (original_length * params->learning_rate *
-                    std::pow(1 - params->learning_rate, tree_index)) / 
-                    (1 - std::pow(1 - params->learning_rate, params->nb_trees));
-            if (number_of_rows == 0) {
+                              std::pow(1 - params->learning_rate, tree_index)) /
+                             (1 - std::pow(1 - params->learning_rate, params->nb_trees));
+            if (number_of_rows == 0)
+            {
                 throw std::runtime_error("Warning: tree is not getting any samples");
             }
         }
@@ -96,42 +108,49 @@ void DPEnsemble::train(DataSet *dataset)
         vector<int> tree_indices(dataset->length);
 
         // gradient-based data filtering
-        if(is_true(params->gradient_filtering)) {
+        if (is_true(params->gradient_filtering))
+        {
 
             // divide samples into rejected/remaining gradients
-            std::vector<int> reject_indices(dataset->length,0), remaining_indices(dataset->length,0);
-            for (int i=0; i<dataset->length; i++) {
+            std::vector<int> reject_indices(dataset->length, 0), remaining_indices(dataset->length, 0);
+            for (int i = 0; i < dataset->length; i++)
+            {
                 double curr_grad = dataset->gradients[i];
-                bool reject = constant_time::logical_or(curr_grad < -params->l2_threshold, curr_grad > params->l2_threshold);
+                bool reject = constant_time::logical_or(curr_grad<-params->l2_threshold, curr_grad> params->l2_threshold);
                 reject_indices[i] = reject;
                 remaining_indices[i] = constant_time::logical_not(reject);
             }
 
             int remaining_count = std::accumulate(remaining_indices.begin(), remaining_indices.end(), 0);
             LOG_INFO("GDF: {1} of {2} rows fulfill gradient criterion",
-                remaining_count, dataset->length);
+                     remaining_count, dataset->length);
 
             /** first use as many "remaining" samples as possible */
 
             // generate random index permutation
             std::vector<int> permutation(dataset->length);
-            std::iota(std::begin(permutation), std::end(permutation), 1);  // [1,2,3,...]
-            if (!VERIFICATION_MODE) {
+            std::iota(std::begin(permutation), std::end(permutation), 1); // [1,2,3,...]
+            if (!VERIFICATION_MODE)
+            {
                 std::random_shuffle(permutation.begin(), permutation.end());
             }
             // zero out all elements that are not part of the remaining array
-            for(auto &elem : permutation){
-                elem *= remaining_indices[elem-1];
+            for (auto &elem : permutation)
+            {
+                elem *= remaining_indices[elem - 1];
             }
-            std::transform(permutation.begin(), permutation.end(), permutation.begin(), [](int &c){return c-1;}); // make it 0-indexed
+            std::transform(permutation.begin(), permutation.end(), permutation.begin(), [](int &c)
+                           { return c - 1; }); // make it 0-indexed
 
             // put corresponding rows into tree_indices
             int taken_rows = 0;
-            for(int i=0; i<dataset->length; i++){
+            for (int i = 0; i < dataset->length; i++)
+            {
                 bool use_row = constant_time::logical_and(permutation[i] != -1, taken_rows < number_of_rows);
-                taken_rows += (int) use_row;
+                taken_rows += (int)use_row;
                 // touch the entire tree_samples vector, to hide which one is added
-                for(int j=0; j < dataset->length; j++){
+                for (int j = 0; j < dataset->length; j++)
+                {
                     tree_indices[j] = constant_time::select(constant_time::logical_and(j == permutation[i], use_row), 1, tree_indices[j]);
                 }
             }
@@ -142,49 +161,57 @@ void DPEnsemble::train(DataSet *dataset)
             LOG_INFO("GDF: filling up with {1} rows (clipping those gradients)", num_additional_samples_required);
 
             // generate random index permutation
-            std::iota(std::begin(permutation), std::end(permutation), 1);  // [1,2,3,...]
-            if (!VERIFICATION_MODE) {
+            std::iota(std::begin(permutation), std::end(permutation), 1); // [1,2,3,...]
+            if (!VERIFICATION_MODE)
+            {
                 std::random_shuffle(permutation.begin(), permutation.end());
             }
             // zero out all elements that are not part of the rejected array
-            for(auto &elem : permutation){
-                elem *= reject_indices[elem-1];
+            for (auto &elem : permutation)
+            {
+                elem *= reject_indices[elem - 1];
             }
-            std::transform(permutation.begin(), permutation.end(), permutation.begin(), [](int &c){return c-1;}); // make it 0-indexed
+            std::transform(permutation.begin(), permutation.end(), permutation.begin(), [](int &c)
+                           { return c - 1; }); // make it 0-indexed
 
             // put corresponding rows into tree_indices
             taken_rows = 0;
-            for(int i=0; i<dataset->length; i++) {
+            for (int i = 0; i < dataset->length; i++)
+            {
                 // use row iff the row is part of "rejected_indices" and we still need more samples
                 bool use_row = constant_time::logical_and(permutation[i] != -1, taken_rows < num_additional_samples_required);
-                taken_rows += (int) use_row;
+                taken_rows += (int)use_row;
                 // clip gradient if this row is used
-                double clipped_gradient = clamp(dataset->gradients[i],-params->l2_threshold, params->l2_threshold);
+                double clipped_gradient = clamp(dataset->gradients[i], -params->l2_threshold, params->l2_threshold);
                 dataset->gradients[i] = constant_time::select(use_row, clipped_gradient, dataset->gradients[i]);
                 // touch the entire tree_indices vector, to hide which one is added
-                for(int j=0; j<dataset->length; j++){
+                for (int j = 0; j < dataset->length; j++)
+                {
                     // if we are at the right element, write 1, otherwise keep content
                     tree_indices[j] = constant_time::select(constant_time::logical_and(j == permutation[i], use_row), 1, tree_indices[j]);
                 }
             }
-            
-        } else {
+        }
+        else
+        {
             // no GDF, just randomly select <number_of_rows> rows. This should not require hardening.
             // Note, this causes the leaves to be clipped after building the tree.
             vector<int> all_indices(dataset->length);
             std::iota(std::begin(all_indices), std::end(all_indices), 0);
-            if (!VERIFICATION_MODE) {
+            if (!VERIFICATION_MODE)
+            {
                 std::random_shuffle(all_indices.begin(), all_indices.end());
             }
-            for(int i=0; i<number_of_rows; i++){       
+            for (int i = 0; i < number_of_rows; i++)
+            {
                 tree_indices[all_indices[i]] = 1;
-            }            
+            }
         }
 
         DataSet tree_dataset = dataset->get_subset(tree_indices);
-        
+
         LOG_DEBUG(YELLOW("Tree {1:2d}: receives pb {2:.2f} and will train on {3} instances"),
-                tree_index, tree_params.tree_privacy_budget, tree_dataset.length);
+                  tree_index, tree_params.tree_privacy_budget, tree_dataset.length);
 
         // build tree
         LOG_INFO("Building dp-tree-{1} using {2} samples...", tree_index, tree_dataset.length);
@@ -199,41 +226,45 @@ void DPEnsemble::train(DataSet *dataset)
     }
 }
 
-
 // Predict values from the ensemble of gradient boosted trees
-vector<double>  DPEnsemble::predict(VVD &X)
+vector<double> DPEnsemble::predict(VVD &X)
 {
-    vector<double> predictions(X.size(),0);
-    for (auto tree : trees) {
+    vector<double> predictions(X.size(), 0);
+    for (auto tree : trees)
+    {
         vector<double> pred = tree.predict(X);
-        
-        std::transform(pred.begin(), pred.end(), 
-            predictions.begin(), predictions.begin(), std::plus<double>());
+
+        std::transform(pred.begin(), pred.end(),
+                       predictions.begin(), predictions.begin(), std::plus<double>());
     }
 
     double innit_score = this->init_score;
     double learning_rate = params->learning_rate;
-    std::transform(predictions.begin(), predictions.end(), predictions.begin(), 
-            [learning_rate, innit_score](double &c){return c*learning_rate + innit_score;});
+    std::transform(predictions.begin(), predictions.end(), predictions.begin(),
+                   [learning_rate, innit_score](double &c)
+                   { return c * learning_rate + innit_score; });
 
     return predictions;
 }
 
-
 void DPEnsemble::update_gradients(vector<double> &gradients, int tree_index)
 {
-    if(tree_index == 0) {
+    if (tree_index == 0)
+    {
         // init gradients
         vector<double> init_scores(dataset->length, init_score);
         gradients = params->task->compute_gradients(dataset->y, init_scores);
-    } else { 
+    }
+    else
+    {
         // update gradients
         vector<double> y_pred = predict(dataset->X);
         gradients = (params->task)->compute_gradients(dataset->y, y_pred);
     }
-    if(VERIFICATION_MODE) {
+    if (VERIFICATION_MODE)
+    {
         double sum = std::accumulate(gradients.begin(), gradients.end(), 0.0);
-        sum = sum < 0 && sum >= -1e-10 ? 0 : sum;  // avoid "-0.00000.. != 0.00000.."
+        sum = sum < 0 && sum >= -1e-10 ? 0 : sum; // avoid "-0.00000.. != 0.00000.."
         VERIFICATION_LOG("GRADIENTSUM {0:.8f}", sum);
     }
 }
