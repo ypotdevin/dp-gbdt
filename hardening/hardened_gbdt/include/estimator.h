214cf4970edce38ace994f6fc3c58ac9b1389cd7
Yannik Potdevin <ypo@informatik.uni-kiel.de>
2022-08-23 08:04:18 +0000

    feat: add simple library interface (C++)

---
+++ hardening/hardened_gbdt/include/estimator.h
@@ -0,0 +1,93 @@
+#ifndef ESTIMATOR_H
+#define ESTIMATOR_H
+
+#include "dp_ensemble.h"
+#include "tree_rejection.h"
+
+namespace dpgbdt
+{
+    /**
+     * @brief A minimal, sci-kit learn like interface for accessing
+     * differentially private gradient boosted decision trees for regression
+     * (not classification).
+     */
+    class Estimator
+    {
+    public:
+        /**
+         * @brief By default, this yields a DP GBDT ensemble with default
+         * settings and DP-rMSE-based tree rejection.
+         */
+        Estimator();
+
+        /**
+         * @brief A DP GBDT according to specified parameters.
+         *
+         * No input validation is performed at this stage. This happens in
+         * Estimator::fit.
+         *
+         * @param rng the random number generator to use for ensemble generation
+         * @param privacy_budget the privacy budget for the ensemble
+         * @param tree_rejector the tree rejection mechanism to use
+         * @param learning_rate the learning rate to use
+         * @param nb_trees the maximal size of the ensemble (if all trees will
+         * be accepted)
+         * @param max_depth the maximal depth per each individual tree
+         * @param min_samples_split minimal amount of samples required to split
+         * a tree node
+         * @param l2_threshold the l2_threshold to use
+         * @param l2_lambda the l2_lambda to use
+         * @param balance_partition whether to balance the partition
+         * @param gradient_filtering whether to perform gradient filtering
+         * @param leaf_clipping whether to perform leaf clipping
+         * @param use_decay whether to use decay
+         */
+        Estimator(
+            std::mt19937 const &rng,
+            double privacy_budget,
+            std::unique_ptr<tree_rejection::TreeRejector> tree_rejector,
+            double learning_rate,
+            int nb_trees,
+            int max_depth,
+            int min_samples_split,
+            double l2_threshold,
+            double l2_lambda,
+            bool balance_partition,
+            bool gradient_filtering,
+            bool leaf_clipping,
+            bool use_decay);
+
+        /**
+         * @brief Perform input validation and fit the DP GBDT to the provided
+         * data.
+         *
+         * @param X the training data input variables
+         * @param y the training data output variable
+         * @param cat_idx which columns (indices) of X are of categorical
+         * nature?
+         * @param num_idx which columns (indices) of X are of numerical nature?
+         * @return the fitted estimator
+         */
+        Estimator &fit(std::vector<std::vector<double>> X, std::vector<double> y, std::vector<int> cat_idx, std::vector<int> num_idx);
+
+        /**
+         * @brief Infer the output variables corresponding to the provided input
+         * variables.
+         *
+         * Note: It is necessary to fit this estimator before calling predict.
+         *
+         * @param X input variables (need to be of the same type is the training
+         * input variables, especially concerning the positioning of categorical
+         * and numerical features).
+         * @return std::vector<double> the inferred output variables
+         */
+        std::vector<double> predict(std::vector<std::vector<double>> X);
+
+    private:
+        std::shared_ptr<ModelParams> params;
+        std::shared_ptr<DPEnsemble> ensemble;
+    };
+
+}
+
+#endif // ESTIMATOR_H
---
+++ hardening/hardened_gbdt/src/estimator.cpp
@@ -0,0 +1,73 @@
+#include "estimator.h"
+
+namespace dpgbdt
+{
+    Estimator::Estimator()
+    {
+        this->params = std::shared_ptr<ModelParams>(new ModelParams());
+        this->params->rng = std::mt19937(std::random_device{}());
+        this->params->privacy_budget = 1.0;
+        this->params->tree_rejector = std::unique_ptr<tree_rejection::DPrMSERejector>(new tree_rejection::DPrMSERejector(0.01, 100.0, 2.0, this->params->rng));
+        this->params->learning_rate = 0.1;
+        this->params->nb_trees = 50;
+        this->params->max_depth = 6;
+        this->params->min_samples_split = 2;
+        this->params->l2_threshold = 1.0;
+        this->params->l2_lambda = 0.1;
+        this->params->balance_partition = true;
+        this->params->gradient_filtering = true;
+        this->params->leaf_clipping = true;
+        this->params->use_decay = false;
+        this->params->task = std::shared_ptr<Task>(new Regression());
+    }
+
+    Estimator::Estimator(
+        std::mt19937 const &rng,
+        double privacy_budget,
+        std::unique_ptr<tree_rejection::TreeRejector> tree_rejector,
+        double learning_rate,
+        int nb_trees,
+        int max_depth,
+        int min_samples_split,
+        double l2_threshold,
+        double l2_lambda,
+        bool balance_partition,
+        bool gradient_filtering,
+        bool leaf_clipping,
+        bool use_decay)
+    {
+        this->params = std::shared_ptr<ModelParams>(new ModelParams());
+        this->params->rng = rng;
+        this->params->privacy_budget = privacy_budget;
+        this->params->tree_rejector = std::move(tree_rejector);
+        this->params->learning_rate = learning_rate;
+        this->params->nb_trees = nb_trees;
+        this->params->max_depth = max_depth;
+        this->params->min_samples_split = min_samples_split;
+        this->params->l2_threshold = l2_threshold;
+        this->params->l2_lambda = l2_lambda;
+        this->params->balance_partition = balance_partition;
+        this->params->gradient_filtering = gradient_filtering;
+        this->params->leaf_clipping = leaf_clipping;
+        this->params->use_decay = use_decay;
+        this->params->task = std::shared_ptr<Task>(new Regression());
+    }
+
+    Estimator &Estimator::fit(std::vector<std::vector<double>> X, std::vector<double> y, std::vector<int> cat_idx, std::vector<int> num_idx)
+    {
+        /* This might lead to problems, since DPEnsemble takes (?) ownership of
+         * passed ModelParams (but does not use shared_pointers instead of raw
+         * pointers). */
+        this->ensemble = std::shared_ptr<DPEnsemble>(new DPEnsemble(this->params.get()));
+        this->params->cat_idx = cat_idx;
+        this->params->num_idx = num_idx;
+        DataSet dataset = DataSet(X, y);
+        this->ensemble->train(&dataset);
+        return *this;
+    }
+
+    std::vector<double> Estimator::predict(std::vector<std::vector<double>> X)
+    {
+        return this->ensemble->predict(X);
+    }
+}
