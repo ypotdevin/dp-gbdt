768a7e7b1c9005cac26a56a3f73b35fd2ffff255
Yannik Potdevin <ypo@informatik.uni-kiel.de>
2022-11-07 13:37:49 +0000

    remove useless file

--- hardening/hardened_gbdt/merge_json.py
+++
@@ -1,9 +0,0 @@
-import json
-import pandas as pd
-
-if __name__ == "__main__":
-    with open("/home/ypo/share/dp-gbdt-evaluation/results.txt") as file:
-        for (i, line) in enumerate(file):
-            if i >= 100:
-                break
-            print(line)
